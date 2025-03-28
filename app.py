927ccbbe9cb704016a0c80eff44f824b7ff4a17b
Vinicius <viniciussggrossi@gmail.com>
2025-03-28 19:20:39 -0300

    algo

--- app.py
+++ app.py
@@ -76,7 +76,7 @@ casa_de_aposta = st.selectbox(
     'Casa de Apostas',
     ['Bet 365', 'Betano', 'Betfair', 'Superbet', 'Estrela Bet', '4Play Bet', 'PixBet',
      'Novibet', 'Sporting Bet', 'Bet7k','Cassino Pix','KTO','Stake', 'BR Bet', 'Aposta tudo','Rei do Pitaco','Bulls Bet', 'Casa de Apostas',
-     'Vera Bet', 'Bateu Bet', 'Betnacional', 'Jogue Facil', 'Jogo de Ouro', 'Pagol','MetGol','UxBet','McGames',
+     'Vera Bet', 'Bateu Bet', 'Betnacional', 'Jogue Facil','Br4 Bet', 'Jogo de Ouro', 'Pagol','MetGol','UxBet','McGames',
      'Seu Bet', 'Bet Esporte', 'BetFast', 'Faz1Bet', 'Esportiva Bet', 'Betpix365',
      'Seguro Bet', 'Outros'],
 )
